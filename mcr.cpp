78fb8eb913181b28a2c818e795cb3c340fe0cfc5
Yitong-web <58107082+Yitong-web@users.noreply.github.com>
2024-09-16 19:32:19 +0800

    Update mcr.cpp

--- mcr.cpp
+++ mcr.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 bool isWin(char game[3][3]){ 
     bool win = false; 
     if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] 
@@ -23,19 +26,22 @@ bool isWin(char game[3][3]){
  
 int main(){ 
     int i, j; 
-    char game[3][3] = {' '}; // Tic-tac-toe 
+    int n;
+    char game[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}}; // Tic-tac-toe
     char player1 = 'X'; 
     char player2 = 'O'; 
     bool turn = true; // false for player 1's turn, true for player 2's turn. Player 1 first. 
     cout << "X = Player 1" << endl << "O = Player 2" << endl; 
-    for (int n=0; n<9; n++){ 
+    for (n=0; n<9; n++){ 
         turn = !turn;  // use the not-operator to change true to false or false to true. 
         if (turn == false) 
             cout << "Player 1: "; 
         else 
             cout << "Player 2: "; 
-        cout << "Which cell to mark? i:[1..3], j:[1..3]: ";  
-        cin >> i >> j; 
+        do {
+                cout << "Which cell to mark? i:[0..2], j:[0..2]: ";
+                cin >> i >> j;
+            } while (game[i][j] != ' ');
         if (turn == false) 
         game[i][j] = 'X';
         else  
@@ -45,7 +51,7 @@ int main(){
             break; // need to terminate the problem 
         } 
     } 
-    if (i==3) // all celles with i=0 to 2 have been inputted above but no winner yet 
+    if (n == 9) // all celles with i=0 to 2 have been inputted above but no winner yet
         cout << "Tie!" << endl; 
  
  // show the game to console 
